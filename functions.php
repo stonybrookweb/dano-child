75050a75de2b0609ecd7f4bdba97a0c14124664c
Alana Martin <alana@stonybrookweb.com>
2017-04-19 19:07:03 -0400

    feat: create new child theme

---
+++ functions.php
@@ -0,0 +1,11 @@
+<?php
+
+// enqueue parent styles
+add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles');
+
+function enqueue_parent_styles(){
+	wp_enqueue_style('parent-style', get_template_directory_uri().'/style.css');
+	
+}
+
+?>
